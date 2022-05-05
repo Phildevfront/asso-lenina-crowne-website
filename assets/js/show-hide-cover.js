fb80e2820d3247592bd2312f264dfca682815b34
Baurens <48996314+Phildevfront@users.noreply.github.com>
2022-05-05 17:04:45 +0200

    DELETE js script not used

--- assets/js/show-hide-cover.js
+++
@@ -1,6 +0,0 @@
-var wrapper = document.getElementById('wrapper');
-function switchVisible() {
-    wrapper.classList.toggle('switched');
-}
-document.getElementById('button').addEventListener('click', switchVisible);
-  
